ccb63a62b5fb35e5eecdbae8790acd985c939bd7
mass-min <koyod111@gmail.com>
2018-12-09 16:50:52 +0800

    rename file

--- scripts/slack_responder.rb
+++ scripts/slack_responder.rb
@@ -1,17 +1,13 @@
-require "bundler/setup"
+require 'bundler/setup'
 require 'slack-ruby-client'
 
-TOKEN = 'xoxb-290786944402-498170702880-IkHBbrHCoV3i1YmpFHa02AO0'
+TOKEN = ENV['SLACK_API_TOKEN']
 
 Slack.configure do |conf|
   conf.token = TOKEN
 end
 
 client = Slack::RealTime::Client.new
-# client = Slack::Web::Client.new
-
-# client.chat_postMessage(channel: '#tokyo-house-keeping', text: 'Test post'
-
 client.on :hello do
   puts "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
 end
